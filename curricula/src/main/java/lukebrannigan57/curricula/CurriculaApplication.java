a63b7445be6225b475e025447eb9ec41ed37bde3
luke.brannigan <luke.brannigan@sarnova.com>
2022-05-31 20:07:30 -0400

    created project

---
+++ curricula/src/main/java/lukebrannigan57/curricula/CurriculaApplication.java
@@ -0,0 +1,13 @@
+package lukebrannigan57.curricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurriculaApplication.class, args);
+    }
+
+}
---
+++ curricula/src/test/java/lukebrannigan57/curricula/CurriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package lukebrannigan57.curricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
